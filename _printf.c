532476e8e197d493988fb4a38f0d3c85b1a7b581
lekba07 <lekbar@bamis.mr>
2025-06-16 10:29:25 +0000

    main.c

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _printf - A simplified printf implementation
+ * @format: The format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0, count = 0;
+    char c, *str;
+
+    if (!format)
+        return (-1);
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        if (format[i] == '%' && format[i + 1])
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    if (!str)
+                        str = "(null)";
+                    while (*str)
+                    {
+                        write(1, str++, 1);
+                        count++;
+                    }
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);
+                    write(1, &format[i], 1);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
+}
+
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life\n");
+    _printf("Character: %c\n", 'S');
+    _printf("String: %s\n", "Go Spurs Go!");
+    _printf("Percentage: %%\n");
+    return 0;
+}
+
